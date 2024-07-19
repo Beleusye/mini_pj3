e8d9e65d682fe285e1f7efa29c53efbcfda9d917
Kpikachu <60080368+Beleusye@users.noreply.github.com>
2024-07-19 16:37:38 +0900

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -4,18 +4,22 @@
 source ./var
 
 # ansible 설치
-sudo apt install -y ansible sshpass
+sudo apt install -y ansible
 
 # ansible 설정 파일, 인벤토리 가져오기
 sudo curl https://raw.githubusercontent.com/Beleusye/mini_pj3/master/ansible.cfg > ~/ansible.cfg
 sudo curl https://raw.githubusercontent.com/Beleusye/mini_pj3/master/inventory > ~/inventory
 
 # ansible-playbook 가져오기
-sudo wget https://raw.githubusercontent.com/Beleusye/mini_pj3/master/ssh_key.yml
-sudo wget https://raw.githubusercontent.com/Beleusye/mini_pj3/master/dhcp.yml
-sudo wget https://raw.githubusercontent.com/Beleusye/mini_pj3/master/ftp.yml
-sudo wget https://raw.githubusercontent.com/Beleusye/mini_pj3/master/nfs.yml
-sudo wget https://raw.githubusercontent.com/Beleusye/mini_pj3/master/k8s_master_node.yml
+sudo wget -N https://raw.githubusercontent.com/Beleusye/mini_pj3/master/ssh_key.yml
+sudo wget -N https://raw.githubusercontent.com/Beleusye/mini_pj3/master/dhcp.yml
+sudo wget -N https://raw.githubusercontent.com/Beleusye/mini_pj3/master/ftp.yml
+sudo wget -N https://raw.githubusercontent.com/Beleusye/mini_pj3/master/nfs.yml
+sudo wget -N https://raw.githubusercontent.com/Beleusye/mini_pj3/master/k8s_master_node.yml
+sudo wget -N https://raw.githubusercontent.com/Beleusye/mini_pj3/master/docker.yml
+
+# sshpass 설치 (SSH 키 배포 시 필요)
+sudo apt install -y sshpass
 
 # ssh 공개 키 배포
 sudo echo "ansible_ssh_pass: ${set_root_password}" > ssh_pass.yml
@@ -38,5 +42,6 @@ sudo ansible-playbook k8s_master_node.yml
 # AWS CLI Configure 보안 상 직접 구성
 echo "Next Step - Configure Yourself
 - aws configure
-- aws eks update-kubeconfig --region [Region] --name [Cluster_Name]"
+- aws eks update-kubeconfig --region [Region] --name [Cluster_Name]
+- ansible-playbook docker.yml"
 
